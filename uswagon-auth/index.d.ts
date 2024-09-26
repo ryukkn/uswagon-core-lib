d8542e0f1ee7229867955398824996e044d5b9b8
ryukkn <126628758+ryukkn@users.noreply.github.com>
2024-09-26 15:07:52 +0800

    auth-01

--- uswagon-auth/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="uswagon-auth" />
-export * from './public-api';
--- uswagon-auth/lib/uswagon-register-button/uswagon-register-button.component.d.ts
+++
@@ -1,5 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class UswagonRegisterButtonComponent {
-    static ɵfac: i0.ɵɵFactoryDeclaration<UswagonRegisterButtonComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<UswagonRegisterButtonComponent, "uswagon-register-button", never, {}, {}, never, never, false, never>;
-}
